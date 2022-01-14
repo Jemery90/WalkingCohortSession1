ee333f6a3648a9fe00454d3df0f148a4a19c6992
Evgenii Kozyrev <evgenii.kozyrev@evgenii.kozyrev.macbook>
2022-01-14 11:39:49 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -2,10 +2,13 @@ describe('fizzbuzz', () => {
     it("given an input of integer 1 a string of 1 is returned",() => {
         expect(fizzBuzz(1)).toBe("1")
     });
+    it("given an input of integer 2 a string of 2 is returned",() => {
+        expect(fizzBuzz(2)).toBe("2")
+    })
 }
 )
 
 function fizzBuzz(int: number): any {
-    throw new Error("Function not implemented.");
-}
+    return "1";
+    }
 

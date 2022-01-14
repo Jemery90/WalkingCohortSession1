6102ef82442e3b030a462a5051bed9d4152670e4
Josh Emery <josh.emery@smartfleet.co.uk>
2022-01-14 10:07:42 +0000

    first commit

---
+++ src/test/example.test.ts
@@ -0,0 +1,11 @@
+describe('fizzbuzz', () => {
+    it("given an input of integer 1 a string of 1 is returned",() => {
+        expect(fizzBuzz(1)).toBe("1")
+    });
+}
+)
+
+function fizzBuzz(int: number): any {
+    throw new Error("Function not implemented.");
+}
+

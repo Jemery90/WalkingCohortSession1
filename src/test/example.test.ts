73f89050c53c239bef3c062737004a8881d2107f
Josh Emery <josh.emery@smartfleet.co.uk>
2022-01-14 11:38:57 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,10 +1,22 @@
 describe('fizzbuzz', () => {
+    describe('boundary checks', () => {
+        it.each([ 
+            [0, new Error("Out of bounds")],
+            [101, new Error("Out of bounds")]
+        ])    
+        ("given an input of integer %i an exception is thrown", (int:number, expected: Error) => {
+            expect(() => fizzBuzz(int)).toThrow(expected);
+        })
+    })
     it.each([
         [1, "1"],
         [2, "2"],
         [3, "fizz"],
         [4, "4"],
-        [5, "fizz"]
+        [5, "buzz"],
+        [6, "fizz"],
+        [10, "buzz"],
+        [15, "fizzbuzz"]
     ])
     ("given an input of integer %i a string of %s is returned", (int:number, expected: string ) => {
         expect(fizzBuzz(int)).toBe(expected);
@@ -13,7 +25,10 @@ describe('fizzbuzz', () => {
 )
 
 function fizzBuzz(int: number): string {
-    if (int === 3) return "fizz";
+    if (int < 1 || int > 100) throw new Error("Out of bounds");    
+    if  (int % 15 === 0) return "fizzbuzz"
+    if (int % 3 === 0) return "fizz";
+    if (int % 5 === 0) return "buzz";
     return `${int}`;
 }
 
